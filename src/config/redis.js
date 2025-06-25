42b2d9dac176c2ad6a6547c97503e72955f30be6
SumeetCodeJC <code.sumeet9@gmail.com>
2025-06-25 15:10:04 +0530

    new commit

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,13 @@
 const { createClient } = require('redis');
+require('dotenv').config(); 
+console.log("Connecting to Redis URL:", process.env.REDIS_URL); 
+const redisClient = createClient({
+  url: process.env.REDIS_URL
+});
 
-const redisClient = createClient();
+redisClient.on('error', (err) => console.error('Redis Error:', err));
 
-redisClient.on('error', (err) => console.error('Redis error:', err));
 redisClient.connect();
 
 module.exports = redisClient;
+
